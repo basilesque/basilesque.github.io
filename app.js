0934448691a886d93785ac0811bfb72031348dc1
Reyhan <reyhantoron@gmail.com>
2023-12-19 19:01:31 +0300

    muted the colors

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ function changeBackground() {
     let redValue = (Math.floor(Math.random() * 256));
     let greenValue = (Math.floor(Math.random() * 256));
     let blueValue = (Math.floor(Math.random() * 256));
-    document.body.style.background = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+    let aValue = (Math.random() * 0.5 + 0.3).toFixed(2);
+    document.body.style.background = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${aValue})`;
     // document.getElementById("randomise-btn").style.color = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 
     let oppositeRed = 255 - redValue;
     let oppositeGreen = 255 - greenValue;
     let oppositeBlue = 255 - blueValue;
-    document.getElementById("randomise-btn").style.background = `rgb(${oppositeRed}, ${oppositeGreen}, ${oppositeBlue})`;
+    document.getElementById("randomise-btn").style.background = `rgba(${oppositeRed}, ${oppositeGreen}, ${oppositeBlue}, ${aValue})`;
 };
