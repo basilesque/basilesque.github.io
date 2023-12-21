185afe9d5e372613970acf50d943c7cdc18d4940
Reyhan <reyhantoron@gmail.com>
2023-12-21 17:12:44 +0300

    aValue renamed to alphaValue

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ function changeBackground() {
     let redValue = (Math.floor(Math.random() * 256));
     let greenValue = (Math.floor(Math.random() * 256));
     let blueValue = (Math.floor(Math.random() * 256));
-    let aValue = (Math.random() * 0.5 + 0.3).toFixed(2);
-    document.body.style.background = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${aValue})`;
+    let alphaValue = (Math.random() * 0.5 + 0.3).toFixed(2);
+    document.body.style.background = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaValue})`;
     // document.getElementById("randomise-btn").style.color = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 
     let oppositeRed = 255 - redValue;
     let oppositeGreen = 255 - greenValue;
     let oppositeBlue = 255 - blueValue;
-    document.getElementById("randomise-btn").style.background = `rgba(${oppositeRed}, ${oppositeGreen}, ${oppositeBlue}, ${aValue})`;
+    document.getElementById("randomise-btn").style.background = `rgba(${oppositeRed}, ${oppositeGreen}, ${oppositeBlue}, ${alphaValue})`;
 };
